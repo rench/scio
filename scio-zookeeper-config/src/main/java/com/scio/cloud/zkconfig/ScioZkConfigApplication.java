68e650fff3848b677a0650d00c0f673ff7d2f964
Wang.ch <Wang.ch@gaiasys.cn>
2019-01-29 08:46:53 +0800

    add scio-zookeeper-config client demo

--- scio-zookeeper-config/src/main/java/com/scio/cloud/zkconfig/ScioZkConfigApplication.java
+++ scio-zookeeper-config/src/main/java/com/scio/cloud/zkconfig/ScioZkConfigApplication.java
@@ -4,24 +4,54 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 /**
  * zookeeper config client demo
+ *
  * <pre>
  * Document: https://cloud.spring.io/spring-cloud-static/spring-cloud-zookeeper/2.1.0.RELEASE/single/spring-cloud-zookeeper.html#spring-cloud-zookeeper-config
+ * Document: https://dzone.com/articles/spring-cloud-config-series-part-2-git-backend
+ * Document: http://www.enriquerecarte.com/2017-07-21/spring-cloud-config-series-introduction
+ * Document: https://dzone.com/articles/spring-cloud-config-part-3-zookeeper-backend
  * Zookeeper: http://zookeeper.apache.org/
  * </pre>
+ *
  * @author wang.ch
  * @date 2019-1-25 15:42:17
  */
 @SpringBootApplication
 @RefreshScope
+@RestController
 public class ScioZkConfigApplication {
 
+  /**
+   *
+   *
+   * <pre>
+   * path store in zookeeper znode: /configuration/scio-zookeeper-config,dev/scio.username
+   * znodedata: username
+   * </pre>
+   */
   @Value("${scio.username}")
   private String username;
-
+  /**
+   *
+   *
+   * <pre>
+   * path store in zookeeper: /configuration/scio-zookeeper-config,dev/scio.password
+   * znodedata: password
+   * </pre>
+   */
   @Value("${scio.password}")
   private String password;
+
+  @RequestMapping("/config")
+  @ResponseBody
+  public String username() {
+    return this.username + '@' + this.password;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(ScioZkConfigApplication.class, args);
